f3541cae6696b253159cecb39a897588cfd5eddc
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-02-12 03:18:07 -0500

    add quincyatt to the roster
    Change-Id: Ice85748b1b66a430254e7e5a9d31a73cc4968a00

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -65,6 +65,7 @@ add_lunch_combo slim_p5100-userdebug
 add_lunch_combo slim_p5110-userdebug
 add_lunch_combo slim_p700-userdebug
 add_lunch_combo slim_pyramid-userdebug
+add_lunch_combo slim_quincyatt-userdebug
 add_lunch_combo slim_shamu-userdebug
 add_lunch_combo slim_sirius-userdebug
 add_lunch_combo slim_skyrocket-userdebug
